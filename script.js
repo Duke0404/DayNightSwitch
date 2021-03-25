45ea02df9a4f6149cb6aaccb92eb03aeecae3478
Duke0404 <devansh0404@gmail.com>
2021-03-25 21:16:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 class themeProp {
-    constructor(name, background, textColor, switchPosition, switchColor) {
+    constructor(name, background, textColor, switchPosition, switchColor, switchIcon) {
         this.properties = [];
         this.properties[0] = name;
         this.properties[1] = background;
         this.properties[2] = textColor;
         this.properties[3] = switchPosition;
         this.properties[4] = switchColor;
+        this.properties[5] = switchIcon;
     }
 }
 
-const light = new themeProp("Light", "images/day.svg", "#454545", "flex-end", "#FFB800");
-const dark = new themeProp("Dark", "images/night.svg", "#fff", "flex-start", "#70869e");
+const light = new themeProp("Light", "images/day.svg", "#454545", "flex-end", "#FFB800", "images/sun.svg");
+const dark = new themeProp("Dark", "images/night.svg", "#fff", "flex-start", "#70869e", "images/moon.svg");
 const root = document.documentElement;
-const themeName = document.getElementById("themeName");
 
 function insertCSS(theme) {
     root.style.setProperty("--background", "url(" + theme.properties[1] + ") no-repeat center center fixed");
     root.style.setProperty("--text-color", theme.properties[2]);
     root.style.setProperty("--switch-position", theme.properties[3]);
     root.style.setProperty("--switch-color", theme.properties[4]);
-    themeName.innerHTML = theme.properties[0];
+    document.getElementById("switchIcon").setAttribute("src", theme.properties[5]);
+    document.getElementById("themeName").innerHTML = theme.properties[0];
 }
 
 function themeSwitch() {
